5616e98f48271dea68874990c5e2f1507df1c6a4
gglipp <gglipp@126.com>
2019-01-07 15:13:49 +0800

    comment

--- echo/src/main/java/com/panlijun/echo/EchoServer.java
+++ echo/src/main/java/com/panlijun/echo/EchoServer.java
@@ -23,13 +23,18 @@ public class EchoServer {
         final EchoServerHandler serverHandler = new EchoServerHandler();
         NioEventLoopGroup loopGroup = new NioEventLoopGroup();
         try {
+            // create serverBootstrap
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(loopGroup)
+                    // assign the channel for nio
                     .channel(NioServerSocketChannel.class)
+                    // assign the InetSocketAddress
                     .localAddress(new InetSocketAddress(8888))
+                    // add the EchoServerHandler to child channel's pipeline
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
+                            // because of the Sharable annotation, all different channels share the same EchoServerHandler instance
                             ch.pipeline().addLast(serverHandler);
                         }
                     });
